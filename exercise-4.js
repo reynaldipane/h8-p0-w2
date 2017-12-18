05cd9504502624da5ba3428f7c9b4be5411e8d60
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2017-12-18 19:20:11 +0700

    Add exercise-4.js

---
+++ exercise-4.js
@@ -0,0 +1,16 @@
+console.log('LOOPING PERTAMA');
+
+i = 2;
+
+while (i <= 20) {
+    console.log(i + ' - I love coding ');
+    i = i + 2;
+}
+
+i = 20;
+
+console.log('LOOPING KEDUA')
+while (i >= 2) {
+    console.log(i + ' - I will become fullstack developer ');
+    i = i - 2;
+}
